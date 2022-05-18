99731df733ae9ed3b1cbb419d70b66f4fa744bb7
Manny Dador <EADador@gmail.com>
2022-05-18 12:28:29 -0400

    skeleton

--- core/models.py
+++ core/models.py
@@ -13,16 +13,21 @@ class Habit(models.Model):
 	name = models.CharField(max_length=200, null=True, blank=True)
 	goal = models.IntegerField
 	unit = models.CharField(max_length=100)
-	created_at = models.DateTimeField(auto_now_add=True)
+	created_at = models.DateField(auto_now_add=True)
 	user = models.ForeignKey('User', null=True, blank=True,on_delete=models.CASCADE, related_name='habits')
 
 	def __str__(self):
-	    return self.title
+	    return self.name
 
 class Record(models.Model):
 	date = models.DateField(auto_now_add=datetime.now)
 	habit = models.ForeignKey('Habit', null=True, blank=True,on_delete=models.CASCADE, related_name='habit_record')
 	user = models.ForeignKey('User', null=True, blank=True,on_delete=models.CASCADE, related_name='habit_user_record')
+	
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(fields=['date','habit'], name='daily')
+		]
 
 	def __str__(self):
-	    return self.title
+	    return self.habit
