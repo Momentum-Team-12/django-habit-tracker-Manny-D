b82f292a54e0a07f3c24fa77d676f8f838a22b84
Manny Dador <EADador@gmail.com>
2022-05-18 12:35:03 -0400

    skeleton v1.5

---
+++ core/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import AbstractUser
+
+# class CreateUserForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = [
+#             'username',
+#             'email',
+#             'password1',
+#             'password2',
+#         ]
